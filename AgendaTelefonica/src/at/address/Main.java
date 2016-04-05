8bace0ba7f632ef20f8069ab8baa379864e0d82b
Lfsobrinho <luisfelipee.sob@gmail.com>
2016-04-05 15:08:41 -0300

    Alteraçao nome main

--- AgendaTelefonica/src/at/address/Main.java
+++ AgendaTelefonica/src/at/address/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.setTitle("AgendaTelefonica");
 
         initRootLayout();
 
